1995e281b73df9986c8d176abd6c9454e53adb57
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2026-01-12 13:35:56 -0800

    Time: 2 ms (89.32%) | Memory: 47.7 MB (78.59%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        //If non zero, move left, 
+        //if we move zeros to right, relative position of non zero will be changed.
+        int nonzeroIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[nonzeroIndex];
+                nums[nonzeroIndex] = temp;
+                nonzeroIndex++;
+            }
+        }
+    }
+}
