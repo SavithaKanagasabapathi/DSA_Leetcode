7d77b56bb8a6f534f8b478297c1b5c6efb8d2925
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2026-01-12 13:38:22 -0800

    Add complexity comments to moveZeroes method
    Added time and space complexity comments to the moveZeroes method.

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -2,6 +2,7 @@ class Solution {
     public void moveZeroes(int[] nums) {
         //If non zero, move left, 
         //if we move zeros to right, relative position of non zero will be changed.
+        //TC-O(n) and SC-O(1)
         int nonzeroIndex = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
