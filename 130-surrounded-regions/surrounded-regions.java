7b16398aaeb7826a7d38a3f37bb6aff2d73d570c
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2026-02-10 15:35:18 -0800

    Time: 1 ms (99.77%) | Memory: 47.9 MB (35.82%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,36 @@
+class Solution {
+    //TC-O(M*N) and SC-O(M*N)
+    //if boundary O present, mark it as # and change inner O to X and finally change # to O
+    public void solve(char[][] board) {
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+        for (int i = 0; i < n; i++) {
+            dfs(board, 0, i);
+            dfs(board, m - 1, i);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int row, int col) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length
+                || board[row][col] != 'O') {
+            return;
+        }
+        board[row][col] = '#';
+        dfs(board, row + 1, col);
+        dfs(board, row - 1, col);
+        dfs(board, row, col + 1);
+        dfs(board, row, col - 1);
+    }
+}
