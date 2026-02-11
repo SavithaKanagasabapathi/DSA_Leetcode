5dca1d3ea3711df3062afb51e260155db577f24b
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2026-02-11 12:28:08 -0800

    Time: 23 ms (98.82%) | Memory: 44.6 MB (6.53%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    //TC-O(V+E) and SC-O(V), map and recursion stack upto height of graph
+    Map<Node, Node> map = new HashMap<>();//outside recursive call
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+        Node clone = new Node(node.val);
+        map.put(node, clone);
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+        return clone;
+    }
+}
